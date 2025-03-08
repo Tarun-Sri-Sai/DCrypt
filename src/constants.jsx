10d0f3b69fe5f599576d3f176f096d7e2deb9892
Tarun-Sri-Sai <tarunsrisaiv03@gmail.com>
2025-03-08 19:17:11 +0530

    Fix constant name for item length

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,3 +1,3 @@
 export const DIRECTORY_KEY = "dcryptDirectory";
-export const MIN_ITEM_NAME_LENGTH = 32;
+export const MAX_ITEM_NAME_LENGTH = 32;
 export const INFO_TIMEOUT = 3000;
--- src/utils.jsx
+++ src/utils.jsx
@@ -1,10 +1,10 @@
-import { MIN_ITEM_NAME_LENGTH } from "./constants";
+import { MAX_ITEM_NAME_LENGTH } from "./constants";
 
 export const isValidName = (name) => {
   return (
     name.trim().length === name.length &&
     name.length > 0 &&
-    name.length <= MIN_ITEM_NAME_LENGTH &&
+    name.length <= MAX_ITEM_NAME_LENGTH &&
     /^[a-zA-Z0-9._-\s]+$/.test(name)
   );
 };
